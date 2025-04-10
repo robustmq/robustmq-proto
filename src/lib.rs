146913f79326e7a342db53998ce79c43d96ea9bd
PorterZhang2021 <porterzhang2021@outlook.com>
2025-04-10 21:53:17 +0800

    style: add proto style

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,82 @@
+// Copyright 2023 RobustMQ Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
     let proto_root = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
     // Declare dependencies for all proto files and directories
-    println!("cargo:rerun-if-changed={}", proto_root.join("protos/journal_server/*.proto").display());
-    println!("cargo:rerun-if-changed={}", proto_root.join("protos/broker_mqtt/*.proto").display());
-    println!("cargo:rerun-if-changed={}", proto_root.join("protos/placement_center/*.proto").display());
-    println!("cargo:rerun-if-changed={}", proto_root.join("protos/prost_validation_types/*.proto").display());
+    println!(
+        "cargo:rerun-if-changed={}",
+        proto_root.join("protos/journal_server/*.proto").display()
+    );
+    println!(
+        "cargo:rerun-if-changed={}",
+        proto_root.join("protos/broker_mqtt/*.proto").display()
+    );
+    println!(
+        "cargo:rerun-if-changed={}",
+        proto_root.join("protos/placement_center/*.proto").display()
+    );
+    println!(
+        "cargo:rerun-if-changed={}",
+        proto_root
+            .join("protos/prost_validation_types/*.proto")
+            .display()
+    );
 
     // Journal Engine
     tonic_build::configure().build_server(true).compile_protos(
         &[
-            proto_root.join("protos/journal_server/proto/admin.proto").to_str().unwrap(),
-            proto_root.join("protos/journal_server/proto/engine.proto").to_str().unwrap(),
-            proto_root.join("protos/journal_server/proto/inner.proto").to_str().unwrap(),
-            proto_root.join("protos/journal_server/proto/record.proto").to_str().unwrap(),
+            proto_root
+                .join("protos/journal_server/proto/admin.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/journal_server/proto/engine.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/journal_server/proto/inner.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/journal_server/proto/record.proto")
+                .to_str()
+                .unwrap(),
         ],
-        &[proto_root.join("protos/journal_server/proto/").to_str().unwrap()],
+        &[proto_root
+            .join("protos/journal_server/proto/")
+            .to_str()
+            .unwrap()],
     )?;
 
     // MQTT Broker
     tonic_build::configure().build_server(true).compile_protos(
         &[
-            proto_root.join("protos/broker_mqtt/proto/admin.proto").to_str().unwrap(),
-            proto_root.join("protos/broker_mqtt/proto/inner.proto").to_str().unwrap(),
+            proto_root
+                .join("protos/broker_mqtt/proto/admin.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/broker_mqtt/proto/inner.proto")
+                .to_str()
+                .unwrap(),
         ],
-        &[proto_root.join("protos/broker_mqtt/proto/").to_str().unwrap()],
+        &[proto_root
+            .join("protos/broker_mqtt/proto/")
+            .to_str()
+            .unwrap()],
     )?;
 
     // Placement Center
@@ -37,15 +89,36 @@ pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
     prost_validate_build::Builder::new().compile_protos_with_config(
         config,
         &[
-            proto_root.join("protos/placement_center/proto/journal.proto").to_str().unwrap(),
-            proto_root.join("protos/placement_center/proto/kv.proto").to_str().unwrap(),
-            proto_root.join("protos/placement_center/proto/mqtt.proto").to_str().unwrap(),
-            proto_root.join("protos/placement_center/proto/inner.proto").to_str().unwrap(),
-            proto_root.join("protos/placement_center/proto/openraft.proto").to_str().unwrap(),
+            proto_root
+                .join("protos/placement_center/proto/journal.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/placement_center/proto/kv.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/placement_center/proto/mqtt.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/placement_center/proto/inner.proto")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/placement_center/proto/openraft.proto")
+                .to_str()
+                .unwrap(),
         ],
         &[
-            proto_root.join("protos/placement_center/proto/").to_str().unwrap(),
-            proto_root.join("protos/prost_validation_types/proto/").to_str().unwrap(),
+            proto_root
+                .join("protos/placement_center/proto/")
+                .to_str()
+                .unwrap(),
+            proto_root
+                .join("protos/prost_validation_types/proto/")
+                .to_str()
+                .unwrap(),
         ],
     )?;
     Ok(())
